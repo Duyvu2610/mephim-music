08be1d719fe7089a0901b2d90b24a2a3baf06b98
Your Name <you@example.com>
2022-04-21 17:34:06 +0700

    upload video

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ window.addEventListener("load", function () {
     const prevButton = document.querySelector(".fa-backward")
     const swapButton = document.querySelector(".fa-shuffle")
     const remaining = document.querySelector(".player-remaining")
-    const duration = document.querySelector(".player-duration")
+    const PlayerDuration = document.querySelector(".player-duration")
     const bar = document.querySelector("#progress-bar")
+    const video = document.querySelector("#myVideo")
     let playing = true;
     playButton.addEventListener("click", handlerPlay)
     let songIndex = 1;
     function handlerPlay() {
         if (playing) {
             song.play();
+            video.play()
             playButton.classList.remove("fa-play");
             playButton.classList.add("fa-pause");
             playing = false;
         } else {
+            video.pause()
             song.pause();
             playButton.classList.remove("fa-pause");
             playButton.classList.add("fa-play");
@@ -29,6 +32,9 @@ window.addEventListener("load", function () {
     prevButton.addEventListener("click", function () {
         handleSong(-1);
     });
+    song.addEventListener("ended", function () {
+        handleSong(1)
+    })
     function handleSong(num) {
         if (num === 1) {
             songIndex++;
@@ -48,5 +54,28 @@ window.addEventListener("load", function () {
             handlerPlay()
         }
     }
+    function displayTimer() {
+        const { duration, currentTime } = song;
+        if (!duration) {
+            PlayerDuration.textContent = "0:00"
+        } else {
+
+            PlayerDuration.textContent = formatTimer(duration - currentTime)
+        }
+        remaining.textContent = formatTimer(currentTime)
+        bar.max = duration
+        bar.value = currentTime
+    }
+    function formatTimer(number) {
+        const minute = Math.floor(number / 60);
+        const second = Math.floor(number - minute * 60)
+        return `${minute}:${second < 10 ? "0" + second : second}`;
+    }
+    bar.addEventListener("change", handleBar)
+    function handleBar() {
+        song.currentTime = bar.value;
+
+    }
+    const timer = setInterval(displayTimer, 900);
 
 })
